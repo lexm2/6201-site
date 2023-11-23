8da87e2de00fe5320be4ba029ac38c7087de1a8d
lexm2 <91219911+lexm2@users.noreply.github.com>
2023-11-23 12:50:59 -0500

    working text reveal

--- components/TextReveal.jsx
+++ components/TextReveal.jsx
@@ -1,57 +1,50 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 function TextReveal( {text, classes} ) {
-    const [lettersRef, setlettersRef] = useArrayRef();
-    const triggerRef = useRef(null)
+  const [lettersRef, setlettersRef] = useArrayRef();
+  const triggerRef = useRef(null);
 
+  function useArrayRef() {
+    const lettersRef = useRef([]);
+    lettersRef.current = [];
+    return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
+  }
 
-    function useArrayRef() {
-        const lettersRef = useRef([]);
-        lettersRef.current = [];
-        return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
-    }
+  gsap.registerPlugin(ScrollTrigger);
 
-    gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const anim = gsap.to(lettersRef.current, {
+      scrollTrigger: {
+        trigger: triggerRef.current,
+        scrub: true,
+        start: "top center",
+        end: "bottom center",
+        //markers: true,
+      },
+      color: getComputedStyle(document.documentElement).getPropertyValue('--text-color'),
+      duration: 5,
+      stagger: 1,
+    });
+    return () => {
+      anim.kill();
+    };
+  }, []);
 
-    useEffect(() => {
-        const anim = gsap.to(
-            lettersRef.current,
-            {
-                scrollTrigger: {
-                    trigger: triggerRef.current,
-                    scrub: true,
-                    start: "top center",
-                    end: "bottom 85%"
-
-                },
-                color: "#2A2A2A",
-                duration: 5,
-                stagger: 1,
-            }
-        );
-        return (() => {
-            anim.kill()
-        })
-    }, []);
-
-    return (
-        <>
-            <div className="spacing-small"></div>
-            <div className={`reveal ${classes}`}>
-                <div ref={triggerRef}>
-                    {text.split("").map((letter, index) => (
-                        <span className="reveal-text" key={index} ref={setlettersRef}>
-                            {letter}
-                        </span>
-                    ))}
-                </div>
-            </div>
-            <div className="spacing"></div>
-        </>
-    )
+  return (
+    <>
+      <div className={`reveal ${classes}`}>
+        <div ref={triggerRef}>
+          {text.split("").map((letter, index) => (
+            <span className="reveal-text" key={index} ref={setlettersRef}>
+              {letter}
+            </span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default TextReveal
+export default TextReveal;
